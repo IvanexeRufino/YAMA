549b2cf0bf1ab749063c39e151b798ec1ba94787
ivanexerufino <ivanexerufino@gmail.com>
2018-11-30 19:53:00 -0300

    hello world

---
+++ helloworld/helloworld.c
@@ -0,0 +1,11 @@
+/*
+ * helloworld.c
+ *
+ *  Created on: 30 nov. 2018
+ *      Author: utnso
+ */
+
+int main(void) {
+
+	printf("hello world");
+}
---
+++ helloworld/helloworld.h
@@ -0,0 +1,13 @@
+/*
+ * helloworld.h
+ *
+ *  Created on: 30 nov. 2018
+ *      Author: utnso
+ */
+
+#ifndef HELLOWORLD_HELLOWORLD_H_
+#define HELLOWORLD_HELLOWORLD_H_
+
+
+
+#endif /* HELLOWORLD_HELLOWORLD_H_ */
